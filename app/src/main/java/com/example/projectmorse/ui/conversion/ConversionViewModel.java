0bc81bb71cb9d39533021d2b069aa3daa99cd644
Jonathon Kirkpatrick <jon.kirkpatrick@sbcglobal.net>
2019-11-10 01:01:58 -0600

    This makes it easier for us to use Javascript

---
+++ app/src/main/java/com/example/projectmorse/ui/conversion/ConversionViewModel.java
@@ -0,0 +1,19 @@
+package com.example.projectmorse.ui.conversion;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ConversionViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ConversionViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
